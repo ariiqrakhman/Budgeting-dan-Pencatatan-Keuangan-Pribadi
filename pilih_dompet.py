176f029f416524d2d822b6fb80565c91376ae344
anggitodwip <126871765+anggitodwip@users.noreply.github.com>
2023-06-01 06:36:24 +0700

    Add files via upload
    cmt_ang

---
+++ pilih_dompet.py
@@ -0,0 +1,45 @@
+import submodules
+
+def pilih_dompet():
+    while True:
+        # Tampilkan nama dompet
+        header, list_dompet = submodules.open_read_csv("dompet.csv")
+        for nama, nominal in list_dompet:
+            print(nama, nominal)
+        print("buat")
+        
+        # Input nama dompet atau buat
+        opsi = [ele[0] for ele in list_dompet] + ["buat"]
+        pilih = submodules.input_of_options("Pilih dompet kena transaksi atau buat dompet",opsi)
+        if pilih != "buat":
+            indeks = opsi.index(pilih)
+            return list_dompet[indeks][0]
+        
+        # Apabila input buat
+        # Masukkan nama dompet
+        nama = submodules.input_normal("Masukkan nama dompet : ")
+        nominal = submodules.input_money("Masukkan nominal awal dompet : ")
+
+        # Konfirmasi input
+        print(f'''Konfirmasi:
+Nama dompet  = {nama}
+Nominal awal = Rp{nominal:,}''')
+
+        konfir_input = submodules.input_of_yatidak("Apakah mau menyimpan dompet? (y/t) ")
+
+        if konfir_input == "y":
+            tulis = [[ nama,nominal ]]
+            submodules.open_append_csv("dompet.csv", tulis)
+
+def tulis_dompet(namadompet, code:str, nominal:int):
+    header, list_dompet = submodules.open_read_csv("dompet.csv")
+
+    for ele in list_dompet:
+        if ele[0] == namadompet:
+            ele[1] = int(ele[1]) + nominal if code == "1" else int(ele[1]) - nominal
+    
+    submodules.open_write_all_csv("dompet.csv",list_dompet, header)
+
+# pilih_dompet()
+
+# tulis_dompet(pilih_dompet(),"1",10000)
---
+++ submodules.py
@@ -0,0 +1,91 @@
+from os import getcwd, path
+from csv import reader, writer
+
+def open_read_csv(filename):
+    filepath = path.join(getcwd(),"files",filename)
+    with open(filepath, "r", newline="") as f:
+        baca = list(reader(f))
+        header = baca[0]
+        content = baca[1:]
+    
+    return header, content
+
+def open_write_all_csv(filename, ds, hd):
+    filepath = path.join(getcwd(),"files",filename)
+    with open(filepath, "w", newline="") as f:
+        tulis = writer(f)
+        tulis.writerow(hd)
+        tulis.writerows(ds)
+
+
+def open_append_csv(filename, ds):
+    filepath = path.join(getcwd(),"files",filename)
+    with open(filepath, "a", newline="") as f:
+            tulis = writer(f)
+            tulis.writerows(ds)
+
+def input_normal(prompt:str):
+    while True:
+        try:
+            ans = input(prompt)
+            break
+        except AssertionError as er:
+            print(er)
+    return ans.lower()
+
+def input_of_options(prompt:str, ls_options:list, errormsg= "Input Tidak Valid!"):
+    while True:
+        try:
+            ans = input(prompt)
+            assert ans.lower() in ls_options, errormsg
+            break
+        except AssertionError as er:
+            print(er)
+    return ans.lower()
+
+def input_of_yatidak(prompt:str, errormsg= "Input Tidak Valid!"):
+    while True:
+        try:
+            ans = input(prompt)
+            assert ans.lower() in ["y", "t"], errormsg
+            break
+        except AssertionError as er:
+            print(er)
+    return ans.lower()
+
+def input_money(prompt:str):
+    while True:
+        try:
+            amount = int(input(prompt))
+            assert amount >= 0, "Uang tidak negatif!"
+            break
+        except AssertionError as er:
+            print(er)
+        except ValueError:
+            print("Masukkan uang dengan benar")
+    return amount
+
+def input_money_w_params(prompt:str, code:str, moneyparam:int):
+    while True:
+        try:
+            amount = input(prompt)
+            assert amount >= 0, "Uang tidak negatif!"
+            assert amount - moneyparam >= 0 if code == "0" else True
+            break
+        except AssertionError as er:
+            print(er)
+    return amount
+
+
+
+# dict1 = [{"nama":"anggito", "umur":19},
+#          {"nama":"ariiq","umur":19}]
+
+# dict2 = [{"nama":"rakha", "umur":19},
+#          {"nama":"dea","umur":19}]
+
+# open_write_all_csv("dompet.csv",dict1)
+# open_append_csv("dompet.csv", dict2)
+
+# fk = open_read_csv("dompet.csv")
+# print(fk)
