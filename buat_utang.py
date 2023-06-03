38567d646a2ce60526067f9ba7c42dc2799bace8
Ariiq Rakhman Setiana <ariiqrakhmanss@student.uns.ac.id>
2023-06-03 13:06:01 +0700

    SP buat_utang

--- buat_utang.py
+++ buat_utang.py
@@ -13,3 +13,25 @@ def rekap_utang(nama:str, dompet:int, nominal:int):
 
 if __name__ == "__main__":
     rekap_utang("utang Dea", "umum", 100000)
+    
+def buat_utang():
+    while True:
+        dompet = input_of_options("Pilih dompet (A/B): ", ["a", "b"])
+        nama_utang = input_normal("Masukkan nama utang: ")
+        nominal_utang = input_money("Masukkan nominal utang: ")
+        finalisasi = input_of_yatidak("Apakah Anda ingin finalisasi pembuatan utang? (y/t): ")
+        
+        if finalisasi == "y":
+            header, content = open_read_csv("rekap_utang.csv")
+            content.append([dompet, nama_utang, nominal_utang])
+            open_write_all_csv("rekap_utang.csv", content, header)
+            print("Utang berhasil dibuat!")
+            break
+        elif finalisasi == "t":
+            buat_lagi = input_of_yatidak("Apakah Anda ingin membuat utang baru? (y/t): ")
+            if buat_lagi == "t":
+                print("Selesai")
+                break
+            elif buat_lagi == "y":
+                continue
+
