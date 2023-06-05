c4cc9666d3a1ffccc60174c6fff46dca606c78e8
Rakha75 <bagusrakha644@student.uns.ac.id>
2023-06-05 11:49:19 +0700

    cmt_rkh

--- bayar_utang.py
+++ bayar_utang.py
@@ -3,11 +3,33 @@ from pilih_dompet import pilih_dompet, tulis_dompet
 from pemasukan_pengeluaran import rekap_pemasukan_pengeluaran
 
 def bayar_utang(nama:str, dompet:int, nominal:int):
-    rekap_pemasukan_pengeluaran("1", "utang", dompet, nominal)
+    #membuka file csv 
+    header,list_utang = submodules.open_read_csv("utang.csv")
+    #memeriksa apakah ada utang dalam daftar
+    if len(list_utang >= 1 ):
+        #menampilkan daftar utang
+        for ele in list_utang:
+            print(ele[0], ele[1])
+        opsi = [ele[0] for ele in list_utang] + ["keluar"]
+        #memilih utang yang akan dibayar
+        while True:
+            utang_bayar = submodules.input_of_options(prompt='input nama utang atau keluar',opsi)
+            if utang_bayar == "keluar":
+                print ("konfirmasi keluar")
+                return
+            #memilih dompet dan nominal
+            dompet, nominal = pilih_dompet()
 
-    toadd = [[ nama, nominal ]]
 
-    submodules.open_append_csv("utang.csv", toadd)
 
-if __name__ == "__main__":
-    bayar_utang("Utang Rakha","umum", 12000)
+
+    else:
+        print('Output"Tidak ada utang"')
+
+
+
+
+bayar_utang()
+
+# if __name__ == "__main__":
+#     bayar_utang("Utang Rakha","umum", 12000)
