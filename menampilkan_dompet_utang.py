d4b7ea28b1639600ccb76e4eb496210a09251855
Rakha75 <bagusrakha644@student.uns.ac.id>
2023-06-13 11:45:39 +0700

    cmt_rakha

--- menampilkan_dompet_utang.py
+++ menampilkan_dompet_utang.py
@@ -5,17 +5,24 @@ def menampilkan_dompet_utang():
     hd_dompet , ls_dompet = submodules.open_read_csv("dompet.csv")
     hd_utang , ls_utang = submodules.open_read_csv("utang.csv")
 
+    # Penyesuaian konten nominal dengan mata uang rupiah
+    ls_dompet = [ [ele[0], f"Rp{int(ele[1]):>10,}"] for ele in ls_dompet ]
+    ls_utang = [ [ele[0], f"Rp{int(ele[1]):>10,}"] for ele in ls_utang ]
+
     # Tampilkan dompet
     if len(ls_dompet) >= 1:
+        print("Daftar Dompet Pengguna :")
         submodules.display_table(ls_dompet, hd_dompet)
+        print()
     else:
         print("\nKamu belum memiliki dompet")
 
     # Tampilkan utang
     if len(ls_utang) >= 1:
+        print("Daftar Utang Pengguna :")
         submodules.display_table(ls_utang, hd_utang)
     else:
-        print("\nSelamat, Kamu tidak memiliki utang!")
+        print("Selamat, Kamu tidak memiliki utang!")
 
 if __name__ == "__main__":
     menampilkan_dompet_utang()
